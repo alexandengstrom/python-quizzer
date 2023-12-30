fd26f4b3b06fda6a6c496cc6a8e70ba7d3353053
Alexander Engström <aleen361@student.liu.se>
2023-12-30 16:44:58 +0100

    Preparing for first deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Question from "./components/Question";
 import Options from "./components/Options";
