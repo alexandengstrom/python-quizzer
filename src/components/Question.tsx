85cae80060bc98de4de0d18dc0e770f28be8c9cd
Alexander Engström <aleen361@student.liu.se>
2024-01-01 21:18:52 +0100

    Implemented support for night mode

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { docco, vs2015 } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 type Props = {
   question: string;
@@ -8,6 +8,23 @@ type Props = {
 };
 
 const Question: React.FC<Props> = ({ question, codeSnippet }) => {
+  const [isDarkMode, setIsDarkMode] = useState(
+    window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+
+  useEffect(() => {
+    const darkModeMediaQuery = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    );
+    const handleDarkModeToggle = (e: MediaQueryListEvent) =>
+      setIsDarkMode(e.matches);
+    darkModeMediaQuery.addEventListener("change", handleDarkModeToggle);
+
+    return () =>
+      darkModeMediaQuery.removeEventListener("change", handleDarkModeToggle);
+  }, []);
+
   return (
     <>
       <div className="question-container">
@@ -15,7 +32,7 @@ const Question: React.FC<Props> = ({ question, codeSnippet }) => {
         <SyntaxHighlighter
           className="code-snippet"
           language="python"
-          style={docco}
+          style={isDarkMode ? vs2015 : docco}
         >
           {codeSnippet}
         </SyntaxHighlighter>
