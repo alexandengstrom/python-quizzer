b6a0866e2e066864f6ee8ed574706c4a8a763e03
Alexander Engström <aleen361@student.liu.se>
2024-01-03 13:33:16 +0100

    Updated test script to work for the new JSON format

--- scripts/validate_json.py
+++ scripts/validate_json.py
@@ -20,16 +20,22 @@ def validate_json(file_path):
                         assert category == question["difficulty"]
                         assert "question" in question
                         assert "codeSnippet" in question
-                        # assert "options" in question
-                        # assert "correctAnswer" in question
                         assert "explanation" in question
+                        assert "correctAnswer" in question
+                        assert "incorrectAnswers" in question
+                        assert question["correctAnswer"] not in question["incorrectAnswers"]
 
                     except AssertionError as err:
                         raise Exception(
                             f"Invalid question data: {err}\n{question}")
 
+        print("JSON is valid!")
+        total_questions = 0
         for category, count in question_count.items():
-            print(f"{category}: {count} questions.")
+            print(f"{category.title()}: {count} questions.")
+            total_questions += count
+
+        print(f"Total: {total_questions} questions")
 
     except json.JSONDecodeError as err:
         raise Exception(f"JSON file is invalid: {err}")
